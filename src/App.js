e4d1cf17b61647c244913f550c90610bfff82374
agubucu <aemirozbek@gmail.com>
2023-07-11 02:35:51 +0300

    logic for a subtraction after another operator

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
     } else { // below here operators are handled
       if(/[+\-x/]/.test(input)){
         if(!result==""){ //if operand is used on the result its set to the total value back again
-          setTotal([result])
+          setTotal(result)
           setResult("")
         }
         // If the input is an operator, add it as a string to the main array
@@ -80,19 +80,42 @@ function App() {
     const operatorArray = [multiply, divide, add, subtract]
     const operatorSymbols = ["x", "/", "+", "-"]
       let newTotal = [...total];
-    if(/[+\-x/]/.test(newTotal[newTotal.length-1])) {
+      if(/[+\-x/]/.test(newTotal[newTotal.length-1]) && /[+\-x/]/.test(newTotal[newTotal.length-2])) { // if the last two characters are operators
+        setTotal(prevArray => [...prevArray.slice(0, -2)])
+        newTotal.pop()
+        newTotal.pop()
+      } else if(/[+\-x/]/.test(newTotal[newTotal.length-1])) { // if the last character is an operator
       setTotal(prevArray => [...prevArray.slice(0, -1)])
       newTotal.pop()
+    }
+
+    // if(/[+\-x/]/.test(newTotal.join(""))) { // if there is a subtraction after another operator in a calculation
+      
+    // } newTotal = [[1], "x", "-", [1]];
+
+    for(let i = 0; i < newTotal.length-2; i++) {
+      for(let j = 0; j < operatorSymbols.length; j++) {
+        if(newTotal[i]===operatorSymbols[j]) {
+          if(newTotal[i+1]==="-") {
+            newTotal.splice(i+1,1);
+            newTotal[i + 1][0] *= -1;
+            setResult(newTotal)
+          }
+        }
+      }
+    }
+    
+    if(!operatorSymbols.some(symbol => newTotal.includes(symbol))){ // if newTotal doesn't include any operator
+      setResult(newTotal)
+      return
     }
     for(let j = 0; j<operatorArray.length; j++) {
 
     while (newTotal.includes(operatorSymbols[j])) {
-      console.log("there is a " +operatorSymbols[j])
       let i = newTotal.indexOf(operatorSymbols[j]);
       const num1 = Number(newTotal[i - 1].join(""));
       const num2 = Number(newTotal[i + 1].join(""));
       newTotal.splice(i - 1, 3, [operatorArray[j](num1, num2)]);
-      console.log("hi"+operatorArray[j](num1, num2))
       setResult(newTotal);
     }
     }
